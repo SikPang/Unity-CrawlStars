0f07513dc795787edd49716174eeb8c8a12ad725
SikPang <87380790+SikPang@users.noreply.github.com>
2026-07-09 13:38:29 +0900

    선택한 캐릭터 PlayerPrefs 저장

--- CrawlStars/Assets/Scripts/Core/CharacterManager.cs
+++ CrawlStars/Assets/Scripts/Core/CharacterManager.cs
@@ -19,6 +19,8 @@ namespace Core {
         public CharacterType MyCharacterType { get; private set; }
         public CharacterInfo.Definition MyCharacter { get; private set; }
 
+        private const string SelectedCharacterTypeKey = "SCTK";
+
         public async UniTask<bool> InitializeAsync() {
             var handle = Addressables.LoadAssetAsync<CharacterInfoSo>("CharacterInfo");
             var res = await handle.ToUniTask();
@@ -32,16 +34,25 @@ namespace Core {
             }
 
             characterInfo = new CharacterInfo(clientCharacterInfoSo);
+
+            // 이전에 저장했던 캐릭터 불러오기
+            int selectedType = PlayerPrefs.GetInt(SelectedCharacterTypeKey);
+            SetMyCharacter((CharacterType)selectedType);
             return true;
         }
 
         public void SetMyCharacter(CharacterType type) {
             MyCharacterType = type;
-            if (characterInfo?.Data == null || !characterInfo.Data.TryGetValue(type, out var definition)) {
-                Debug.LogError($"CharacterManager.SetMyCharacter::there is no data for {type}");
-                return;
+            MyCharacter = GetCharacterInfo(type);
+
+            if (MyCharacter == null) {
+                Debug.LogError($"CharacterManager.SetMyCharacter::there is no data for {type}, so fallback to {CharacterType.A} type");
+                type = CharacterType.A;
+                MyCharacterType = type;
+                MyCharacter = GetCharacterInfo(type);
             }
-            MyCharacter = definition;
+
+            PlayerPrefs.SetInt(SelectedCharacterTypeKey, (int)type);
         }
 
         public IReadOnlyDictionary<CharacterType, CharacterInfo.Definition> GetCharacterInfo() {
@@ -49,7 +60,7 @@ namespace Core {
         }
 
         public CharacterInfo.Definition GetCharacterInfo(CharacterType type) {
-            if (!characterInfo.Data.TryGetValue(type, out var definition)) {
+            if (characterInfo?.Data == null || !characterInfo.Data.TryGetValue(type, out var definition)) {
                 Debug.LogError($"CharacterManager.GetCharacterInfoAsync::there is no data for {type}");
                 return null;
             }
